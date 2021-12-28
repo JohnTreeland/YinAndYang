0ed1a53c0ac008592be74814691a499b1411f979
Eliana Bedoya <elbedoyaga@unal.edu.co>
2021-12-28 17:43:49 -0500

    Base de juego creada y movimiento basico personajes

---
+++ Assets/Scenes/Scripts/BlackMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    Rigidbody2D rb;
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float blackXdir = Input.GetAxis("Horizontal Negro");
+
+        rb.velocity = new Vector2 (blackXdir*speed, rb.velocity.y);
+
+        if (Input.GetKey(KeyCode.UpArrow) && IsGrounded()) 
+        {rb.velocity = new Vector2 (rb.velocity.x,jumpForce);}
+        
+    }
+
+    bool IsGrounded() {
+    Vector2 position = transform.position;
+    Vector2 direction = Vector2.down;
+    float distance = 0.1f;
+    
+    RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+    if (hit.collider != null) {
+        return true;
+    }
+    
+    return false;
+    }
+}
---
+++ Assets/Scenes/Scripts/WhiteMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    Rigidbody2D rb;
+    public LayerMask groundLayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float whiteXdir = Input.GetAxis("Horizontal Blanco");
+
+        rb.velocity = new Vector2 (whiteXdir*speed, rb.velocity.y);
+
+        if (Input.GetKey(KeyCode.W) && IsGrounded()) 
+        {rb.velocity = new Vector2 (rb.velocity.x,jumpForce);}
+
+    }
+
+  
+
+bool IsGrounded() {
+    Vector2 position = transform.position;
+    Vector2 direction = Vector2.down;
+    float distance = 0.1f;
+    
+    RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+    if (hit.collider != null) {
+        return true;
+    }
+    
+    return false;
+}
+}
+
+
